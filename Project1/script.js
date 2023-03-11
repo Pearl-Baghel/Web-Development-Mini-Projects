f85ea9c91f6cbefd1b0918aa9707aa5ccc7efad7
Pearl Baghel <89574835+Pearl-Baghel@users.noreply.github.com>
2023-03-11 14:59:00 +0530

    commit

--- Project1/script.js
+++ Project1/script.js
@@ -8,7 +8,6 @@ let progressbar = document.getElementById("progressbar");
 let gif = document.getElementById('gif');
 let songitem = Array.from(document.getElementsByClassName('songitem'));
 
-let playpause = document.getElementsByClassName('far fa-play-circle')
 
 let songs = [
     { songName: "Perfect", filePath: "song/0.mp3 ", coverPath: "cover/0.jpg " },
@@ -19,10 +18,28 @@ let songs = [
     { songName: "BIBA", filePath: "song/5.mp3 ", coverPath: "cover/5.jpg " }
 ]
 songitem.forEach((element,i) => {
-    console.log(element , i);
+    // console.log(element , i);
     element.getElementsByTagName('img')[0].src = songs[i].coverPath;
     element.getElementsByClassName('songname')[0].innerText = songs[i].songName;
+
 });
+
+const makeAllPlays = ()=>{
+    Array.from(document.getElementsByClassName('songitemplay')).forEach(( element) =>{
+        element.classList.remove('fa-pause-circle');
+    element.classList.add('fa-play-circle');
+})
+}
+
+Array.from(document.getElementsByClassName('songitemplay')).forEach((element)=>{
+    element.addEventListener('click', (e)=>{
+        makeAllPlays();
+        e.target.classList.remove('fa-play-circle');
+        e.target.classList.add('fa-pause-circle');
+    
+    })
+})
+
 
 // audioElement.play();
 //Handle play pause click
